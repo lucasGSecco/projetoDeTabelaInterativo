43143ec872c1cc523d38489e89b6b430b3aaf9c2
luqinha <email@email.com>
2024-02-13 20:15:00 -0300

    funcoes novas

--- scriptTable.js
+++ scriptTable.js
@@ -1,45 +1,4 @@
-//lista para armazenar os jogadores
-var jogadores = []
-
-function adicionarPlayer(){
-    //variavel que pega o nome do jogador novo
-    var player = document.querySelector("#player").value;
-
-    //console.log(player);
-
-    //classe para criar novos jogadores
-    class players {
-        nome;
-        pontos = 0;
-
-
-        constructor(nome, pontos){
-            this.nome = nome
-            this.pontos = pontos
-        }
-    }
-    
-    
-    var playerT = new players(player,0);
-
-    
-    playerT.nome = player;
-
-    jogadores.push(playerT);
-    
-    console.log(jogadores);
-    
-}
-
-console.log(jogadores)
-
-function criarTabela(){
-    var tabela = document.createElement("table")
-    var thead = document.createElement("thead");
-    var tbody=document.createElement("tbody");
-}
-
-/*function criarTabela(conteudo) {
+function criarTabela(conteudo) {
     var tabela = document.createElement("table");
     var thead = document.createElement("thead");
     var tbody=document.createElement("tbody");
@@ -64,4 +23,6 @@ function criarTabela(){
     [2,    "cristian", 16],
     [3,    "pedro",    10],
     [4,    "henrique", 10]
-  ]));*/
+  ]));
+
+  
