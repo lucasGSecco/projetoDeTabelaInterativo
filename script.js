1c68733a7b8157fa5a91c3e064975dcb8d520780
lucas galvao secco <lucasgsecco@gmail.com>
2024-08-19 16:28:15 -0300

    adição da lógica da tabela

--- script.js
+++ script.js
@@ -1,67 +1,69 @@
-//lista para armazenar os jogadores
 var jogadores = []
 
 function adicionarPlayer(){
     //variavel que pega o nome do jogador novo
     var player = document.querySelector("#player").value;
 
-    //console.log(player);
+    //verificar se o valor de palyer não é vazio
 
-    //classe para criar novos jogadores
-    class players {
-        nome;
-        pontos = 0;
+    if(player === ''){
+        console.log('coloque um valor válido')
+    } else{
+      
+    
+    
+
+      //classe para criar novos jogadores
+      class players {
+          nome;
+          pontos = 0;
 
 
-        constructor(nome, pontos){
-            this.nome = nome
-            this.pontos = pontos
-        }
+          constructor(nome, pontos){
+              this.nome = nome
+              this.pontos = pontos
+          }
+      }
+      
+      
+      var playerT = new players(player,0);
+
+      
+      playerT.nome = player;
+
+      jogadores.push(playerT);
+
     }
-    
-    
-    var playerT = new players(player,0);
-
-    
-    playerT.nome = player;
-
-    jogadores.push(playerT);
-    
+   
     console.log(jogadores);
     
+}
+
+function limparPlayer(){
+  var player = document.querySelector("#player").value;
+
+  player = '';
 }
 
 console.log(jogadores)
 
 function criarTabela(){
-    var tabela = document.createElement("table")
-    var thead = document.createElement("thead");
-    var tbody=document.createElement("tbody");
+    var tbody = document.getElementById('tbody')
+
+    tbody.innerText = ''
+
+    for(let i = 0; i < jogadores.length; i++){
+      let tr = tbody.insertRow();
+
+      let td_nome = tr.insertCell();
+
+      let td_pontos = tr.insertCell();
+
+      td_nome.innerText = jogadores[i].nome
+
+      td_pontos.innerText = jogadores[i].pontos
+    }
+    
 }
 
-/*function criarTabela(conteudo) {
-    var tabela = document.createElement("table");
-    var thead = document.createElement("thead");
-    var tbody=document.createElement("tbody");
-    var thd=function(i){return (i==0)?"th":"td";};
-    for (var i=0;i<conteudo.length;i++) {
-      var tr = document.createElement("tr");
-      for(var o=0;o<conteudo[i].length;o++){
-        var t = document.createElement(thd(i));
-        var texto=document.createTextNode(conteudo[i][o]);
-        t.appendChild(texto);
-        tr.appendChild(t);
-      }
-      (i==0)?thead.appendChild(tr):tbody.appendChild(tr);
-    }
-    tabela.appendChild(thead);
-    tabela.appendChild(tbody);
-    return tabela;
-  }
-  document.getElementById("tabela").appendChild(criarTabela([
-    ["id", "nome",     "idade"],
-    [1,    "matheus",  16],
-    [2,    "cristian", 16],
-    [3,    "pedro",    10],
-    [4,    "henrique", 10]
-  ]));*/
+console.log(jogadores)
