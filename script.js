b30e79fcb35e9ad3ac42882e4d3369ca6ffdc739
lucasGSecco <lucasgsecco@gmail.com>
2025-02-03 21:32:33 -0300

    atualizações

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function adicionarPlayer(){
     //verificar se o valor de player não é vazio
 
     if(player === ''){
-        console.log('coloque um valor válido')
+        alert('coloque um valor válido')
     } else{
       
     
@@ -36,8 +36,13 @@ function adicionarPlayer(){
     }
    
     console.log(jogadores);
-    
+
+
+
+  criarTabela();
+
 }
+
 ///função de limpar o input 
 function limparPlayer(){
   var player = document.querySelector("#player").value;
@@ -47,24 +52,28 @@ function limparPlayer(){
 
 console.log(jogadores)
 
-// função inserir os dados na tabela
+    // função inserir os dados na tabela
 function criarTabela(){
-    var tbody = document.getElementById('tbody')
-
-    tbody.innerText = ''
-
-    for(let i = 0; i < jogadores.length; i++){
+  var tbody = document.getElementById('tbody')
+  
+  tbody.innerText = ''
+  
+  for(let i = 0; i < jogadores.length; i++){
       let tr = tbody.insertRow(); //insert.Row = insere linha na tabela
-
+  
       let td_nome = tr.insertCell();  //insertCell = insere dados na linha
-
+  
       let td_pontos = tr.insertCell();
-
+  
       td_nome.innerText = jogadores[i].nome
-
+  
       td_pontos.innerText = jogadores[i].pontos
     }
-    
+      
 }
 
 console.log(jogadores)
+
+function apagar(){
+  jogadores.pop()
+}
